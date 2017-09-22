4a1ff64428fa5495068c6fbab82224d482dfcb0b
padeen <paulo.deen@gmail.com>
2017-09-22 20:31:59 +0200

    Add ruby book to fixtures. Add @title to product_controller test.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,9 +3,10 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+		@title = "The Great Book #{rand(1000)}"
   end
 
-  test "should get index" do
+	test "should get index" do
     get products_url
     assert_response :success
   end
@@ -15,9 +16,18 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create product" do
+	test "should create product" do 
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      
+    post products_url, params: {    
+        product: { 
+          description: @product.description,
+          image_url: @product.image_url,
+          price: @product.price,
+          title: @title,
+        }
+      }
+
     end
 
     assert_redirected_to product_url(Product.last)
@@ -33,8 +43,17 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+	test "should update product" do
+
+    patch product_url(@product), params: {
+        product: {
+          description: @product.description,
+          image_url: @product.image_url,
+          price: @product.price,
+          title: @title,
+        }
+      }
+
     assert_redirected_to product_url(@product)
   end
 
